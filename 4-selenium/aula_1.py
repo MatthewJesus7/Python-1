79ad351c0218244711df98d430fdb814254cd535
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-09-08 17:48:06 -0300

    funcionando 90%, sem a função de links

--- 4-selenium/aula_1.py
+++ 4-selenium/aula_1.py
@@ -18,79 +18,83 @@ urls = [element.get_attribute("href") for element in elementos_links]
 cards = []  # Lista para armazenar todos os dados coletados
 
 for index, url in enumerate(urls):
-    driver.execute_script("window.open('');") 
-    driver.switch_to.window(driver.window_handles[-1])
-    driver.get(url)
-    
-    try:
-        itens_compra = driver.find_elements(By.CLASS_NAME, "item_compra")
-        
-        for item in itens_compra:
-            try:
-                shop_logo_div = item.find_element(By.CLASS_NAME, "shop_logo")
-                img_element = shop_logo_div.find_element(By.TAG_NAME, "img")
-                loja_nome = img_element.get_attribute("alt")
-                
-                if "Amazon" in loja_nome:
-                    print(f"'Amazon' encontrado na página {index + 1}. Abrindo oferta da Amazon.")
+    if index % 2 == 0:
+        driver.execute_script("window.open('');") 
+        driver.switch_to.window(driver.window_handles[-1])
+        driver.get(url)
 
-                    oferta_link = item.find_element(By.CSS_SELECTOR, ".green_button").get_attribute("href")
+        try:
+            itens_compra = driver.find_elements(By.CLASS_NAME, "item_compra")
+            
+            for item in itens_compra:
+                try:
+                    shop_logo_div = item.find_element(By.CLASS_NAME, "shop_logo")
+                    img_element = shop_logo_div.find_element(By.TAG_NAME, "img")
+                    loja_nome = img_element.get_attribute("alt")
                     
-                    driver.execute_script(f"window.open('{oferta_link}');")
-                    driver.switch_to.window(driver.window_handles[-1]) 
-                    
-                    # Aguardar que a nova página da Amazon seja carregada
-                    WebDriverWait(driver, 20).until(EC.url_contains("amazon"))
-                    
-                    try:
-                        # Extração de dados da página da Amazon com tempos de espera maiores
-                        title = WebDriverWait(driver, 20).until(
-                            EC.presence_of_element_located((By.ID, 'productTitle'))
-                        ).text
+                    if "Amazon" in loja_nome:
+                        print(f"'Amazon' encontrado na página {index + 1}. Abrindo oferta da Amazon.")
 
-                        price = WebDriverWait(driver, 20).until(
-                            EC.presence_of_element_located((By.CLASS_NAME, 'best-offer-name'))
-                        ).text
-
-                        price_whole = WebDriverWait(driver, 20).until(
-                            EC.presence_of_element_located((By.CLASS_NAME, 'a-price-whole'))
-                        ).text
+                        oferta_link = item.find_element(By.CSS_SELECTOR, ".green_button").get_attribute("href")
                         
-                        price_fraction = WebDriverWait(driver, 20).until(
-                            EC.presence_of_element_located((By.CLASS_NAME, 'a-price-fraction'))
-                        ).text
+                        driver.execute_script(f"window.open('{oferta_link}');")
+                        driver.switch_to.window(driver.window_handles[-1]) 
                         
-                        image_element = WebDriverWait(driver, 20).until(
-                            EC.presence_of_element_located((By.CSS_SELECTOR, 'li.imageThumbnail img'))
-                        )
-                        image_url = image_element.get_attribute('src')
-
-                        # Formar o preço completo
-                        total_price = f"R$ {price_whole},{price_fraction}"
-
-                        # Armazenar os dados em uma lista de cartões
-                        cards.append({'title': title, 'price': price, 'total_price': total_price, 'image_url': image_url})
-
-                        print(f"Dados coletados da página {index + 1}: {cards[-1]}")
+                        # Aguardar que a nova página da Amazon seja carregada
+                        WebDriverWait(driver, 20).until(EC.url_contains("amazon"))
                         
-                    except TimeoutException:
-                        print(f"Timeout ao carregar elementos na página da Amazon (página {index + 1}).")
-                    
-                    break
-                    
-            except NoSuchElementException:
-                print(f"Elemento não encontrado em um item na página {index + 1}, pulando item...")
-    
-    except Exception as e:
-        print(f"Erro na página {index + 1}: {e}")
+                        try:
+                            # Extração de dados da página da Amazon com tempos de espera maiores
+                            title = WebDriverWait(driver, 20).until(
+                                EC.presence_of_element_located((By.ID, 'productTitle'))
+                            ).text
 
-    driver.close()
-    driver.switch_to.window(driver.window_handles[-1])
+                            price = WebDriverWait(driver, 20).until(
+                                EC.presence_of_element_located((By.CLASS_NAME, 'best-offer-name'))
+                            ).text
+
+                            price_whole = WebDriverWait(driver, 20).until(
+                                EC.presence_of_element_located((By.CLASS_NAME, 'a-price-whole'))
+                            ).text
+                            
+                            price_fraction = WebDriverWait(driver, 20).until(
+                                EC.presence_of_element_located((By.CLASS_NAME, 'a-price-fraction'))
+                            ).text
+                            
+                            image_element = WebDriverWait(driver, 20).until(
+                                EC.presence_of_element_located((By.CSS_SELECTOR, 'li.imageThumbnail img'))
+                            )
+                            image_url = image_element.get_attribute('src')
+
+                            # Formar o preço completo
+                            total_price = f"R$ {price_whole},{price_fraction}"
+
+                            # Armazenar os dados em uma lista de cartões
+                            cards.append({'title': title, 'price': price, 'total_price': total_price, 'image_url': image_url})
+
+                            print(f"Dados coletados da página {index + 1}: {cards[-1]}")
+                            
+                        except TimeoutException:
+                            print(f"Timeout ao carregar elementos na página da Amazon (página {index + 1}).")
+
+                        driver.close() 
+                        driver.switch_to.window(driver.window_handles[-1])
+
+                        driver.close()
+                        driver.switch_to.window(driver.window_handles[0])
+
+                        break
+                        
+                except NoSuchElementException:
+                    print(f"Elemento não encontrado em um item na página {index + 1}, pulando item...")
+
+        except Exception as e:
+            print(f"Erro na página {index + 1}: {e}")
 
 # Passo 5: Salvar todos os dados coletados em um arquivo CSV
 if cards:
     with open('data.csv', 'a', newline='', encoding='utf-8') as file:
-        writer = csv.DictWriter(file, fieldnames=['title', 'price', 'image_url'])
+        writer = csv.DictWriter(file, fieldnames=['title', 'price', 'total_price', 'image_url'])
         writer.writeheader()  # Escreve os cabeçalhos do CSV
         writer.writerows(cards)  # Escreve os dados no CSV
     print(f"Dados salvos no arquivo CSV: {cards}")

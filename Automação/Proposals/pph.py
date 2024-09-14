c8f62ddd9f473b9a05aa9c9f5f245736b7a04c05
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-09-14 17:24:03 -0300

    proposals1.0

--- Automação/Proposals/pph.py
+++ Automação/Proposals/pph.py
@@ -13,6 +13,8 @@ driver = webdriver.Chrome(service=service)
 # Acessa o site
 driver.get("https://www.peopleperhour.com/freelance-jobs/technology-programming?experienceLevel=1%2C2")
 
+# &page=2
+
 # Carrega os cookies
 cookies = pickle.load(open("cookies/pph_cookies.pkl", "rb"))
 for cookie in cookies:
@@ -25,6 +27,10 @@ driver.refresh()
 
 # Define os seletores corretos para as propostas
 painel_de_propostas = driver.find_elements(By.CLASS_NAME, "list⤍List⤚3R-r9")
+
+links_das_propostas = driver.find_elements(By.CLASS_NAME, "item__url⤍ListItem⤚20ULx")
+urls = [link_da_proposta.get_attribute("href") for link_da_proposta in links_das_propostas]
+
 propostas = []
 
 try:
@@ -35,67 +41,6 @@ try:
 except Exception as e:
     print(f"Erro ao obter as propostas: {e}")
 
-# Suas habilidades e habilidades que você não possui
-my_skills = [
-    'frontend',
-    'html',
-    'css',
-    'javascript',
-    'react',
-    'firebase',
-    'python',
-    'git',
-    'github',
-    'selenium',
-    'web scraping',
-    'website'
-    'automation',             
-    'integration',            
-    'user experience',
-    'frontend',
-    'website design',  
-]
-not_my_skills = [
-    'django',
-    'shopify', 
-    'email template design',
-    'google analytics',
-    'clay ai',
-    'crm system',
-    'zoho lead management',
-    'google merchant',
-    'wordpress',
-    'excel',
-    'data visualization',     
-    'seo optimization'
-]
+print(propostas, urls)
 
-# Função para pontuar as propostas
-def pontuar_proposta(proposta):
-    pontos = 0
-    # Verifica as habilidades que você tem
-    for skill in my_skills:
-        if skill.lower() in proposta.lower():
-            pontos += 1
-
-    # Verifica as habilidades que você não tem e subtrai pontos
-    for skill in not_my_skills:
-        if skill.lower() in proposta.lower():
-            pontos -= 1
-
-    return pontos
-
-# Pontua as propostas
-propostas_com_pontos = []
-for proposta in propostas:
-    pontos = pontuar_proposta(proposta)
-    propostas_com_pontos.append((proposta, pontos))
-
-# Ordena as propostas por pontuação (maior para menor)
-propostas_com_pontos.sort(key=lambda x: x[1], reverse=True)
-
-print("Propostas classificadas por pontuação (maior para menor):")
-for proposta, pontos in propostas_com_pontos:
-    print(f"Pontuação: {pontos} - Proposta: {proposta}")
-    
 driver.quit()
